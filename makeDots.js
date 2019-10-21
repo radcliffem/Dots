c994d6518bd3536137bbe0a81e4d80bf37d0d65e
radcliffem <mary.l.radcliffe@gmail.com>
2019-10-21 13:47:57 -0400

    Add files via upload

--- makeDots.js
+++ makeDots.js
@@ -10,6 +10,7 @@ document.getElementById("startButton").onclick = function(){
 		e.style.display="none";
 	}
 	drawDots(width,height);
+	boxes = [...Array(width)].map(e => Array(height));
 	makeBoxes(width, height);
 }
 
@@ -38,14 +39,12 @@ function makeBoxes(width, height){
 	for(var i=0;i<width-1;i++){
 		for(var j=0;j<height-1;j++){
 			var box = {};
-			box.x = i;
-			box.y = j;
 			box.left=false;
 			box.right=false;
 			box.top=false;
 			box.bottom=false;
 			box.value = 1;
-			boxes.push(box);
+			boxes[i][j]=box;
 		}
 	}
 }
@@ -112,6 +111,9 @@ function makeHorizontal(i, j){
 	ctx.moveTo((i+1)*(horizontal_space+2*radius)-radius,(j+1)*(vertical_space+2*radius)-radius);
 	ctx.lineTo((i+2)*(horizontal_space+2*radius)-radius,(j+1)*(vertical_space+2*radius)-radius);
 	ctx.stroke();
+	
+	changeTop(i,j);
+	changeBottom(i,j-1);
 }
 
 
@@ -127,4 +129,42 @@ function makeVertical(i, j){
 	ctx.lineTo((i+1)*(horizontal_space+2*radius)-radius,(j+2)*(vertical_space+2*radius)-radius);
 	ctx.stroke();
 	
+	changeLeft(i,j);
+	changeRight(i-1,j);
+}
+
+function changeTop(i,j){
+	boxes[i][j].top = true;
+	checkBox(i,j);
+}
+
+function changeBottom(i,j){
+	boxes[i][j].bottom =true;
+	checkBox(i,j);
+}
+
+function changeLeft(i,j){
+	boxes[i][j].left=true;
+	checkBox(i,j);
+}
+
+function changeRight(i,j){
+	boxes[i][j].right=true;
+	checkBox(i,j);
+}
+
+function checkBox(i,j){
+	box=boxes[i][j];
+	if(box.left & box.right & box.top & box.bottom){
+		var canvas = document.getElementById("playCanvas")
+		var ctx = canvas.getContext("2d");
+		var horizontal_space = (canvas.width - 6*width)/(width+1)
+		var vertical_space = (canvas.height-6*height)/(height+1)
+		
+		ctx.save();
+		ctx.translate((i+1)*(horizontal_space+2*radius)-radius, (j+1)*(vertical_space+2*radius)-radius);
+		ctx.fillStyle = "blue";
+		ctx.fillRect(0,0,horizontal_space+2*radius, vertical_space+2*radius);
+		ctx.restore();
+	}
 }
